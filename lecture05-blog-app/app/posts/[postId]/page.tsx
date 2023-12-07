c1f4b281a572015b06ca4460c5be0f6edc2ce803
Paras Rawat <drago138150@gmail.com>
2023-12-07 17:50:49 +0530

    lecture06 done

---
+++ lecture05-blog-app/app/posts/[postId]/page.tsx
@@ -0,0 +1,51 @@
+import getFormattedDate from "@/lib/getFormattedDate";
+import { getPostData, getSortedPostsData } from "@/lib/posts";
+import Link from "next/link";
+import { notFound } from "next/navigation";
+
+type Props = {
+  params: {
+    postId: string;
+  };
+};
+
+export function generateMetadata({ params: { postId } }: Props) {
+  const posts = getSortedPostsData(); //deduped!
+
+  const post = posts.find((post) => post.id === postId);
+  if (!post) {
+    return {
+      title: "Post Not Found",
+    };
+  }
+
+  return {
+    title: post.title,
+  };
+}
+
+export default async function Post({ params: { postId } }: Props) {
+  console.log(postId);
+  const posts = getSortedPostsData(); //deduped!
+  console.log(posts);
+  if (!posts.find((post) => post.id === postId)) {
+    return notFound();
+  }
+
+  const { title, date, contentHtml } = await getPostData(postId);
+
+  const pubDate = getFormattedDate(date);
+
+  return (
+    <main className="px-6 prose prose-xl prose-slate dark:prose-invert mx-auto">
+      <h1 className="text-3xl mt-4 mb-0">{title}</h1>
+      <p className="mt-0">{pubDate}</p>
+      <article>
+        <section dangerouslySetInnerHTML={{ __html: contentHtml }} />
+        <p>
+          <Link href="/">← Back to home</Link>
+        </p>
+      </article>
+    </main>
+  );
+}
